0c24fa72d0a493c1649ee6432ec32eaf34f892a0
Marcos <alemayed@amazon.com>
2023-06-02 09:24:25 -0700

    FIX: Fixed basic plugin for cwl-sink

---
+++ data-prepper-plugins/cwl-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/CwlSinkConfig.java
@@ -0,0 +1,26 @@
+package org.opensearch.dataprepper.plugins.sink;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
+
+public class CwlSinkConfig {
+
+    @JsonProperty("path")
+    @NotEmpty
+    private String path = "src/resources/file-test-sample-output.txt";
+
+    @JsonProperty("aws-user")
+    @NotEmpty
+    private String awsUser = "testUser";
+
+    @JsonProperty("aws-cred")
+    private int awsCredential = 45;
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getAwsUser() { return awsUser; }
+
+    public int getAwsCredential() { return awsCredential; }
+}
