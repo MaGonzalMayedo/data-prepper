a913fc5a5aa4da897a52ee12792708d12721acbe
Marcos_Gonzalez_Mayedo <alemayed@amazon.com>
2023-06-13 10:43:25 -0700

    Added ClientConfigTest class
    Signed-off-by: Marcos_Gonzalez_Mayedo <alemayed@amazon.com>

---
+++ data-prepper-plugins/cwl-sink/src/test/java/org/opensearch/dataprepper/plugins/sink/configuration/ClientConfigTest.java
@@ -0,0 +1,28 @@
+package org.opensearch.dataprepper.plugins.sink.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.opensearch.dataprepper.plugins.sink.ClientConfig;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ClientConfigTest {
+    @Test
+    void check_null_log_group_test() {
+        assertThat(new ClientConfig().getLogGroup(), equalTo(null));
+    }
+    @Test
+    void check_null_log_stream_test() {
+        assertThat(new ClientConfig().getLogStream(), equalTo(null));
+    }
+
+    @Test
+    void check_default_batch_size() {
+        assertThat(new ClientConfig().getBatchSize(), equalTo(10));
+    }
+
+    @Test
+    void check_default_retry_count() {
+        assertThat(new ClientConfig().getRetryCount(), equalTo(10));
+    }
+}
