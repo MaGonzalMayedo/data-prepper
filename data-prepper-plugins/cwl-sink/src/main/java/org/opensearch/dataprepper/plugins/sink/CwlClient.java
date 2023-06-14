5dc21b4b547243ed871df9ad9ca901321cd36ee7
Marcos_Gonzalez_Mayedo <alemayed@amazon.com>
2023-06-13 23:50:21 -0700

    Added the base code for sending log data -> Needs testing
    Signed-off-by: Marcos_Gonzalez_Mayedo <alemayed@amazon.com>

--- data-prepper-plugins/cwl-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/CwlClient.java
+++ data-prepper-plugins/cwl-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/CwlClient.java
@@ -1,5 +1,19 @@
 package org.opensearch.dataprepper.plugins.sink;
 
+import org.opensearch.dataprepper.model.event.Event;
+import org.opensearch.dataprepper.model.record.Record;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.services.cloudwatch.endpoints.internal.Value;
+import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
+import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClientBuilder;
+import software.amazon.awssdk.services.cloudwatchlogs.model.CloudWatchLogsException;
+import software.amazon.awssdk.services.cloudwatchlogs.model.InputLogEvent;
+import software.amazon.awssdk.services.cloudwatchlogs.model.PutLogEventsRequest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * CwlClient is an interface class that simplifies method calls to post logs to
  * CloudWatch logs. It receives a collection of events and interprets them into
@@ -12,12 +26,47 @@ public class CwlClient {
     private final String logStream;
     private final int batchSize;
     private final int retryCount;
+    private final CloudWatchLogsClient cloudWatchLogsClient;
+
+
     public CwlClient(String logGroup, String logStream, int batchSize, int retryCount) {
         this.logGroup = logGroup;
         this.logStream = logStream;
         this.batchSize = batchSize;
         this.retryCount = retryCount;
+        cloudWatchLogsClient = CloudWatchLogsClient.builder()
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
     }
 
+    /**
+     * Function handles the packaging of events into log events before sending a bulk request:
+     * @param logs Collection of Record events which hold log data.
+     */
+    public void pushLogs(Collection<Record<Event>> logs) {
 
+        ArrayList<InputLogEvent> logList = new ArrayList<>();
+
+        for (Record<Event> singleLog: logs) {
+            InputLogEvent tempLogEvent = InputLogEvent.builder()
+                    .message(singleLog.getData().toJsonString())
+                    .timestamp(System.currentTimeMillis())
+                    .build();
+            logList.add(tempLogEvent);
+        }
+        try {
+            //TODO: Can place this in a different method for clarity.
+            PutLogEventsRequest putLogEventsRequest = PutLogEventsRequest.builder()
+                    .logEvents(logList)
+                    .logGroupName(logGroup)
+                    .logStreamName(logStream)
+                    .build();
+
+            cloudWatchLogsClient.putLogEvents(putLogEventsRequest);
+        } catch (CloudWatchLogsException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            throw new RuntimeException();
+        }
+
+    }
 }
