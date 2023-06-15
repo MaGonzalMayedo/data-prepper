6a996a91df388dee509f0890e998afd62a28f665
Marcos_Gonzalez_Mayedo <alemayed@amazon.com>
2023-06-15 15:03:25 -0700

    Added Buffer and BufferFactory Interfaces with InMemoryBuffer
    Signed-off-by: Marcos_Gonzalez_Mayedo <alemayed@amazon.com>

---
+++ data-prepper-plugins/cwl-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/buffer/Buffer.java
@@ -0,0 +1,20 @@
+package org.opensearch.dataprepper.plugins.sink.buffer;
+
+import org.opensearch.dataprepper.model.event.Event;
+
+import java.io.IOException;
+
+/**
+ * Buffer that handles the temporary storage of
+ * events. It isolates the implementation of system storage.
+ */
+public interface Buffer {
+    /**
+     * Size of buffer in bytes.
+     * @return
+     */
+    int getEventCount();
+    long getDuration();
+    void writeEvent(Event event) throws IOException;
+    Event getEvent();
+}
---
+++ data-prepper-plugins/cwl-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/buffer/InMemoryBuffer.java
@@ -0,0 +1,57 @@
+package org.opensearch.dataprepper.plugins.sink.buffer;
+
+import com.google.common.base.Stopwatch;
+import org.apache.commons.lang3.time.StopWatch;
+import org.opensearch.dataprepper.model.event.Event;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * InMemoryBuffer is a simple memory based queue container
+ * for recording information in case an error occurs during log publishing.
+ * TODO: Can inject a threshold for how much data can be held. (Constructor and variable add-on)
+ * TODO: Can potentially inject a more suitable byte-array alongside the threshold (above) to recover bytes
+ * TODO: of an event rather the storing the entire event objects.
+ */
+public class InMemoryBuffer implements Buffer {
+    private static ArrayList<Event> eventBuffer;
+    private final StopWatch stopwatch;
+
+    /**
+     * Instantiating a new buffer means clearing the old one!
+     * Suitable for memory based storage.
+     */
+    InMemoryBuffer() {
+        eventBuffer.clear();
+        stopwatch = new StopWatch();
+        stopwatch.start();
+    }
+
+    @Override
+    public int getEventCount() {
+        return eventBuffer.size();
+    }
+
+    @Override
+    public long getDuration() {
+        return stopwatch.getTime(TimeUnit.SECONDS);
+    }
+
+    /**
+     * Write byte array representing a single event into byteArray.
+     * @param event Event to be stored in buffer.
+     * @throws IOException
+     */
+    @Override
+    public void writeEvent(Event event) throws IOException {
+        eventBuffer.add(event);
+    }
+
+    @Override
+    public Event getEvent() {
+        return eventBuffer.get(0);
+    }
+}
